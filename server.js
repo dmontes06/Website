2236500b355b1a9d0e8b8dddbe9274ec5f464d84
David Montes <hp52330@gmail.com>
2025-07-16 11:42:53 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/submit", (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log("New submission:");
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Message:", message);
+
+  res.json({ message: "Your message has been received!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
